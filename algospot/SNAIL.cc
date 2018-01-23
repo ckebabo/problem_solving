051e8574373a3655def0adc7a88061bac3017c66
worlddolt <worlddolt@synapsoft.co.kr>
2018-01-23 11:37:05 +0900

    [algospot/SNAIL] wa / dp / cpp / 16ms

--- algospot/SNAIL.cc
+++ algospot/SNAIL.cc
@@ -7,13 +7,13 @@ int main() {
 	int C;
 	cin >> C;
 
-	float t[1002][1002] = {{0,}};
+	float t[1002][2002] = {{0,}};
 	t[0][0] = 1;
 	t[1][1] = 0.25; t[1][2] = 0.75;
 
 	for(int i=1;i<=1000;i++) {
 		t[i][i] = t[i-1][i-1]*0.25;
-		for(int j=i+1;j<=1001;j++) {
+		for(int j=i+1;j<=2001;j++) {
 			t[i][j] = t[i-1][j-2]*0.75 + t[i-1][j-1]*0.25;
 		}
 	}
@@ -22,7 +22,7 @@ int main() {
 		int n,m;
 		cin >> n >> m;
 
-		printf("%.10f\n", accumulate(t[m]+n, t[m]+1002, 0.));
+		printf("%.10f\n", accumulate(t[m]+n, t[m]+2002, 0.));
 	}
 
 	return 0;
