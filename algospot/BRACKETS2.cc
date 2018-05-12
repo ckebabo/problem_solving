296e930d9a10718ec138bc85397524d29ea4c860
worlddolt <worlddolt@synapsoft.co.kr>
2018-05-12 22:14:16 +0900

    [algospot/BRACKETS2] re / cpp

---
+++ algospot/BRACKETS2.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	int C;
+	cin >> C;
+	while(C--) {
+		string s;
+		cin >> s;
+
+		stack<char> st;
+		for(auto c : s) {
+			if(string("({[").find(c) != string::npos) {
+				st.push(c);
+			} else {
+				char ch = st.top();
+				if(!((ch=='(' && c==')') ||
+					(ch=='{' && c=='}') ||
+					(ch=='[' && c==']'))) {
+					break;
+				}
+				st.pop();
+			}
+		}
+
+		if(st.empty()) cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
+
+	return 0;
+}
