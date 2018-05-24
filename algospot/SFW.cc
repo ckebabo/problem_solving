9bd9293c6c623410c37c0251b545fce731a9b91b
ckebabo <ckebabo@gmail.com>
2018-05-25 02:41:45 +0900

    [algospot/SFW] wa / cpp / 4ms

---
+++ algospot/SFW.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--) {
+		int N,M,r0,c0,r,c,A,B;
+		cin >> N >> M >> r0 >> c0 >> r >> c >> A >> B;
+		cout << (abs(r-r0)+(A-1))/A + (abs(c-c0)+(B-1))/B << endl;
+	}
+
+	return 0;
+}
