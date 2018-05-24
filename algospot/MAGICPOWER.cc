7854cdf44e04630c064f4ba0b1d0349ad970497b
ckebabo <ckebabo@gmail.com>
2018-05-25 02:54:31 +0900

    [algospot/MAGICPOWER] wa / cpp / 4ms

---
+++ algospot/MAGICPOWER.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--) {
+		int N,M;
+		cin >> N >> M;
+
+		priority_queue<int> q;
+		for(int i=0;i<N;i++) {
+			int k;
+			cin >> k;
+			q.push(k);
+		}
+
+		int ans=0;
+		while(M--) {
+			int k = q.top(); q.pop();
+			ans += k;
+			if(k>1) q.push(k-1);
+		}
+		cout << ans << endl;
+	}
+
+	return 0;
+}
+
