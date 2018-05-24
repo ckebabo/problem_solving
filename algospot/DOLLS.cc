5b49e5864aec5b01b73bc19db6af1f7703d15551
ckebabo <ckebabo@gmail.com>
2018-05-24 14:23:42 +0900

    [algospot/DOLLS] rte(divide by zero) / cpp

--- algospot/DOLLS.cc
+++ algospot/DOLLS.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <algorithm>
 
 using namespace std;
@@ -11,35 +12,38 @@ int main() {
 		int n,m;
 		cin >> n >> m;
 
-		vector<int> v(n);
-		for(auto &a : v) cin >> a;
-
-		vector<int> s(v);
-		sort(begin(s), end(s));
+		int v[100001] = {0,};
+		map<int,int> mm;
+		for(int i=0;i<n;i++) {
+			scanf("%d", v+i);
+			mm[v[i]] += 1;
+		}
 
 		int sum = 0;
+		int cnt = 0;
 		int ans[100001] = {0,};
-		while(m) {
-			const int k = s.front() - sum;
-			if(k * s.size() <= m) {
-				m -= k * s.size();
-				sum += k;
-				s.erase(s.begin());
-
-				for(int i=0; i<n; i++) {
-					if(v[i] >= k) { v[i]-=k; ans[i] += k; }
-				}
-			} else {
-				const int p = m / s.size();
-				int r = m % s.size();
-
-				for(int i=0; i<n; i++) {
-					if(v[i] > 0) { v[i]-=p; ans[i] += p; m -= p; }
-				}
-				for(int i=0; r>0; i++) {
-					if(v[i] > 0) { v[i]-=1; ans[i] += 1; m -= 1; r -= 1; }
-				}
+		for(const auto &entry : mm) {
+			const int k = entry.first - sum;
+			if(k * (n-cnt) > m) {
+				break;
 			}
+			m -= k*(n-cnt);
+			sum += k;
+			cnt += entry.second;
+		}
+		for(int i=0; i<n; i++) {
+			ans[i] = min(sum, v[i]);
+			v[i] = max(0, v[i]-sum);
+		}
+
+		const int p = m / (n-cnt);
+		int r = m % (n-cnt);
+
+		for(int i=0; i<n; i++) {
+			if(v[i] > 0) { v[i]-=p; ans[i] += p; m -= p; }
+		}
+		for(int i=0; r>0; i++) {
+			if(v[i] > 0) { v[i]-=1; ans[i] += 1; m -= 1; r -= 1; }
 		}
 
 		for(int i=0;i<n;i++) printf("%d ", ans[i]); puts("");
