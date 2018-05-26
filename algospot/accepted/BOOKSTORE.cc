e0c8c91eec1794a856d7a584ceb93b761797b6e8
ckebabo <ckebabo@gmail.com>
2018-05-27 03:51:51 +0900

    [algospot/BOOKSTORE] solved / cpp / 276ms

--- algospot/accepted/BOOKSTORE.cc
+++ algospot/accepted/BOOKSTORE.cc
@@ -22,13 +22,14 @@ int main() {
 
 		int ans = 2000000;
 		for(int i=0;i<M;i++) {
-			int s = 0, pp = 0;
-			sort(begin(p[i]), end(p[i]));
-			for(int j=1;j<N;j++) {
-				s += p[i][j].second;
-				pp += p[i][j].first;
+			int now=0, pp=0;
+			sort(begin(p[i]), end(p[i]), greater<pair<int,int>>());
+			for(int j=0;j<N;j++) {
+				const int cost = max(p[i][j].second - pp, 0);
+				now += cost;
+				pp = pp - p[i][j].second + cost + p[i][j].first;
 			}
-			ans = min(ans, s+p[i][0].second-pp);
+			ans = min(ans, now);
 		}
 
 		cout << ans << endl;
